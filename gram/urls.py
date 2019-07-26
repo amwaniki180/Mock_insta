3290c86d50bb651b6da537daf4b6ab0cc74feb95
Antony <amwaniki180@gmail.com>
2019-07-26 16:38:54 +0300

    added index.js

--- gram/urls.py
+++ gram/urls.py
@@ -3,7 +3,7 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
+    url('^$',views.home,name = 'welcome'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
