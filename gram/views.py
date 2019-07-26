7165d7f617ddf8d6d4979547130fd10d59805e9d
Antony <amwaniki180@gmail.com>
2019-07-26 18:21:12 +0300

    added attribute mine

--- gram/views.py
+++ gram/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import authenticate,login,logout
 from .models import *
+from .forms import *
 from django.shortcuts import render, get_object_or_404, redirect
 # Create your views here.
 @login_required(login_url='/accounts/login/')
@@ -17,3 +18,13 @@ def home(request):
         if form.is_valid():
             request.user.profile.post(form)
     return render(request, 'landing.html', locals())
+
+@login_required(login_url='/accounts/login/')
+def mine(request):
+    images = request.user.profile.posts.all()
+    user_object = request.user
+    user_images = user_object.profile.posts.all()
+    user_saved = [save.photo for save in user_object.profile.saves.all()]
+    user_liked = [like.photo for like in user_object.profile.mylikes.all()]
+    print(user_liked)
+    return render(request, 'myprofile.html', locals())\
