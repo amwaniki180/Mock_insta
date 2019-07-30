8a27f48732aa216a0f5c225ada4b876e7695a01a
Antony <amwaniki180@gmail.com>
2019-07-30 12:12:54 +0300

    post.html

--- insta/settings.py
+++ insta/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+# from decouple import config,Csv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -34,7 +35,7 @@ INSTALLED_APPS = [
     'gram.apps.GramConfig',
     'bootstrap4',
     'bootstrap3',
-    'utils',
+    # 'utils',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
